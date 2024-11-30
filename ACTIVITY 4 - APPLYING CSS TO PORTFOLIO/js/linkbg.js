ac16416e7b319546046d978a4b610a8e4676e03e
Harshmellow01 <202202150@cityofmalabonuniversity.edu.ph>
2024-11-29 22:19:31 -0800

    IPT_ACTIVITY

---
+++ ACTIVITY 4 - APPLYING CSS TO PORTFOLIO/js/linkbg.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //collects data from aside .click .link
+    const links = document.querySelectorAll("aside .click .link");
+
+    links.forEach(link => {
+        link.addEventListener("click", () => {
+            // Remove the active class from all links
+            links.forEach(item => item.classList.remove("active"));
+
+            // Add the active class to the clicked link
+            link.classList.add("active");
+        });
+    });
+});
